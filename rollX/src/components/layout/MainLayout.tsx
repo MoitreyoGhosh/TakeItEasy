fcd2c4e1bf2172b3930ffc43e1ccf039846385a4
MoitreyoGhosh <moitreyoghosh@gmail.com>
2026-04-27 20:14:01 +0530

    feat(ui): enhanced base layout and add reusable UI components

--- rollX/src/components/layout/MainLayout.tsx
+++ rollX/src/components/layout/MainLayout.tsx
@@ -8,7 +8,7 @@ export default function MainLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex min-h-screen w-full flex-col">
+    <div className="flex min-h-screen w-full flex-col items-center">
       <Header />
       <main className="flex-1 p-4 lg:p-8">{children}</main>
       <Footer />
