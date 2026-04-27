8996758d9010bd34280fa29502f8e58d9a03d3a8
MoitreyoGhosh <moitreyoghosh@gmail.com>
2026-04-27 20:19:48 +0530

    fix(header): adjust max-width of header container for better responsiveness

--- rollX/src/components/layout/Header.tsx
+++ rollX/src/components/layout/Header.tsx
@@ -5,7 +5,7 @@ import UserNav from "./UserNav";
 export default function Header() {
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 px-4 lg:px-8">
-      <div className="container flex h-14 max-w-screen-2xl items-center">
+      <div className="container flex h-14 max-w-full items-center">
         <div className="mr-4 flex">
           <Link href="/" className="mr-6 flex items-center space-x-2">
             <span className="font-bold sm:inline-block">takeiteasy</span>
